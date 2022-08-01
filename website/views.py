9f4e566140bb6a624453fd73811ff8f6ece51860
xJMarie <juliana.marie39@gmail.com>
2022-08-01 20:26:37 +0800

    Added settings page

--- website/views.py
+++ website/views.py
@@ -15,3 +15,7 @@ def about():
 @views.route('/login')
 def login():
     return render_template("login.html")
+
+@views.route('/settings')
+def settings():
+    return render_template("settings.html")
