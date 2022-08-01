27f3af84c950f5bc135420ef8029f270aa07e6ce
wappints <59228576+wappints@users.noreply.github.com>
2022-08-01 11:38:48 +0800

    display error msgs

--- website/auth.py
+++ website/auth.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, flash
 
 auth = Blueprint('auth', __name__)        # Blueprint of the application.
 
@@ -10,8 +10,25 @@ def about():
 # Log in
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
-    data = request.form
-    print(data)
+    if request.method == 'POST':
+        email = request.form.get('registerEmail')
+        firstName = request.form.get('registerFName')
+        lastName = request.form.get('registerLName')
+        password = request.form.get('registerPassword')
+        password_confirm = request.form.get('registerRepeatPassword')
+        print(email)
+        if len(email) < 4:
+            flash('Email is less than 4 characters', category = 'error')
+        elif len(password) < 7:
+            flash('Password must be at least 7 characters.', category = 'error') 
+        elif password != password_confirm:
+            flash('Password does not match.', category = 'error') 
+        else:
+            flash('Account created!', category='success')
+    else:
+        data = request.form
+        print(data)
+        print("JABOL")
     return render_template("login.html", boolean = True)
 
 # Log out
@@ -22,21 +39,7 @@ def logout():
 # Sign up
 @auth.route('/signup', methods=['GET', 'POST'])
 def sign_up():
-    if request.method == 'POST':
-        email = request.form.get('email')
-        firstName = request.form.get('firstName')
-        lastName = request.form.get('lastName')
-        password = request.form.get('password')
-        password_confirm = request.form.get('password_confirm')
-
-        if email.indexOf('@') != -1:
-            flash('Input is not a valid email.', category = 'error')
-        elif password < 7:
-            flash('Password must be at least 7 characters.', category = 'error') 
-        elif password != password_confirm:
-            flash('Password does not match.', category = 'error') 
-        else:
-            flash('Account created!', category='success')
+    print("PASS1")
 
     return render_template("signup.html")
 
